c243ac6ffde89dc3858b4617fef0e5dcda14fe9a
Elia <elia@Elias-MacBook-Air.local>
2019-06-14 15:16:06 +0700

    convert

--- server/controllers/cryptoController.js
+++ server/controllers/cryptoController.js
@@ -4,6 +4,7 @@ class Controller{
     static getCoinDetail(req,res){
         axios({
             method:'get',
+<<<<<<< HEAD
             url:'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=ab5ef428-29ae-4a60-8a99-6df243c5d5e8&limit=10',
         })
         .then(({data}) => {
@@ -24,6 +25,9 @@ class Controller{
         axios({
             method:'get',
             url:`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=ab5ef428-29ae-4a60-8a99-6df243c5d5e8&slug=${req.body.slug}`,
+=======
+            url:'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=4444ccd2-957a-4ad3-8fb1-c2a2de49342b&limit=10',
+>>>>>>> convert
         })
         .then(({data}) => {
             res.status(200).json(data.data)
@@ -38,7 +42,11 @@ class Controller{
     static searchCrypto(req,res){
         axios({
             method:'get',
+<<<<<<< HEAD
             url:`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=e9b78dec-342a-428b-b838-8d5680552cbe&slug=${req.query.coin}`,
+=======
+            url:`https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=4444ccd2-957a-4ad3-8fb1-c2a2de49342b&slug=${req.body.slug}`,
+>>>>>>> convert
         })
         .then(({data}) => {
             res.status(200).json(data.data)
@@ -46,6 +54,22 @@ class Controller{
         .catch((err) => {
             res.status(500).json({
                 message:'Internal Server Error'
+            })
+        })
+    }
+
+    static exchange(req, res) {
+        axios({
+            method: 'get',
+            url: `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.params.symbol}&convert=IDR`,
+            headers: {'X-CMC_PRO_API_KEY': `1d0a0b91-0d1a-4619-acf4-51b7a0ac24ba`}
+        })
+        .then(({data}) => {
+            res.status(200).json(data)
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: err
             })
         })
     }
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,6 +4,10 @@ const cryptoController = require('../controllers/cryptoController')
 
 route.use(authenticate)
 route.get('/crypto',cryptoController.getCoinDetail)
+<<<<<<< HEAD
 route.get('/search',cryptoController.searchCrypto)
+=======
+route.get('/crypto/convert/:symbol',cryptoController.exchange)
+>>>>>>> convert
 route.post('/cryptometa',cryptoController.getCoinMeta)
 module.exports = route
